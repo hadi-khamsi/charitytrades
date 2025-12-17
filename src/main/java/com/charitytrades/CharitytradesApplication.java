7621e4bd218d29dcbd30f7aa6705e3fd134d2e6e
Hadi Khamsi <hadik8360@gmail.com>
2025-12-16 22:15:44 -0600

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/charitytrades/CharitytradesApplication.java
@@ -0,0 +1,13 @@
+package com.charitytrades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharitytradesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharitytradesApplication.class, args);
+	}
+
+}
